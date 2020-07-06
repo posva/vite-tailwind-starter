69cda3e1750026deb07eacc0a2606c01b0e2d075
Santi <scros97@gmail.com>
2020-07-06 23:14:05 +0200

    refactor: use TailwindCSS Purge (#47)

--- postcss.config.js
+++ postcss.config.js
@@ -1,23 +1,6 @@
-const purgecss = require('@fullhuman/postcss-purgecss')({
-  // Specify the paths to all of the template files in your project
-  content: [
-    './index.html',
-    './src/App.vue',
-    './src/components/**/*.vue',
-    './src/views/**/*.vue',
-    './src/main.js',
-    './src/**/*.js',
-    // etc.
-  ],
-
-  // Include any special characters you're using in this regular expression
-  defaultExtractor: (content) => content.match(/[\w-/.:]+(?<!:)/g) || [],
-})
-
 module.exports = {
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
-    ...(process.env.NODE_ENV == 'production' ? [purgecss] : []),
-  ],
+  ]
 }
