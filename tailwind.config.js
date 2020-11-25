3d18720dd6d414399eedc1b05eef62085c726233
Eduardo San Martin Morote <posva@users.noreply.github.com>
2020-11-25 12:02:17 +0100

    feat: upgrade tailwind to v2 (#120)
    Co-authored-by: Ben Bjurstrom <ben@jelled.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   purge: {
     enabled: process.env.NODE_ENV === 'production',
+    // classes that are generated dynamically, e.g. `rounded-${size}` and must
+    // be kept
+    safeList: [],
     content: [
       './index.html',
       './src/**/*.vue',
@@ -12,13 +15,10 @@ module.exports = {
   },
   theme: {
     extend: {
+      fontWeight: ['hover', 'focus'],
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
     },
-  },
-  future: {
-    removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
   },
 }
