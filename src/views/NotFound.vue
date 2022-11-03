7548af24eb31a9375c0f53de664c7f97f4e57898
Milo Delgado <milo@fetch.ly>
2022-11-03 15:04:53 -0500

    Remove Duplicate sm padding definitions

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="px-4 py-6 mt-10  sm:px-0 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28"
+    class="px-4 py-6 mt-10 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28"
   >
     <div class="sm:text-center lg:text-left">
       <h2
